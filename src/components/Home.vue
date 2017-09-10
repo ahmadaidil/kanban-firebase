883a9ad4178f6b2f66227cd16563425aa07eda89
ahmadaidil <ahmdaidil@gmail.com>
2017-09-10 13:31:55 +0700

    layouting

---
+++ src/components/Home.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="home">
+    <h1>aedel Kanban</h1>
+    <button class="btn btn-default pull-left"><span class="glyphicon glyphicon-plus"></span></button>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'home',
+  data() {
+    return {
+      msg: 'Welcome to Your Vue.js App',
+    };
+  },
+};
+</script>
+
+<style scoped>
+@import url('https://fonts.googleapis.com/css?family=Libre+Barcode+39+Text');
+h1 {
+  color: #e3e3e3;
+  margin-top: -15px;
+  margin-bottom: 35px;
+  font-family: 'Libre Barcode 39 Text', cursive;
+}
+</style>
---
+++ src/components/Kanban.vue
@@ -0,0 +1,93 @@
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="col-md-4">
+        <div class="panel panel-warning">
+          <div class="panel-heading">
+            <h3 class="panel-title">Todo</h3>
+          </div>
+          <div class="panel-body">
+            <div class="panel panel-default">
+              <a href="" class="pull-right"><span class="glyphicon glyphicon-remove"></span></a>
+              <div class="panel-heading" id="headcontent">Panel heading</div>
+              <div class="panel-body" id="bodycontent">
+                Panel content
+                <div class="content-fluid">
+                  <div class="row">
+                    <div class="col-md-6">
+                      
+                    </div>
+                    <div class="col-md-6">
+                      <button class="btn btn-default"><span class="glyphicon glyphicon-chevron-right"></span></button>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-4">
+        <div class="panel panel-primary">
+          <div class="panel-heading">
+            <h3 class="panel-title">Doing</h3>
+          </div>
+          <div class="panel-body">
+            <div class="panel panel-default">
+              <a href="" class="pull-right"><span class="glyphicon glyphicon-remove"></span></a>
+              <div class="panel-heading">Panel heading</div>
+              <div class="panel-body" id="bodycontent">
+                Panel content
+                <div class="content-fluid">
+                  <div class="row">
+                    <div class="col-md-6">
+                      <button class="btn btn-default"><span class="glyphicon glyphicon-chevron-left"></span></button>
+                    </div>
+                    <div class="col-md-6">
+                      <button class="btn btn-default"><span class="glyphicon glyphicon-chevron-right"></span></button>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-4">
+        <div class="panel panel-success">
+          <div class="panel-heading">
+            <h3 class="panel-title">Done</h3>
+          </div>
+          <div class="panel-body">
+            <div class="panel panel-default">
+              <a href="" class="pull-right"><span class="glyphicon glyphicon-remove"></span></a>
+              <div class="panel-heading">Panel heading</div>
+              <div class="panel-body" id="bodycontent">
+                Panel content
+                <div class="content-fluid">
+                  <div class="row">
+                    <div class="col-md-6">
+                      <button class="btn btn-default"><span class="glyphicon glyphicon-chevron-left"></span></button>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+};
+</script>
+
+<style scoped>
+#bodycontent {
+  border: 1px solid #485563;
+}
+</style>
